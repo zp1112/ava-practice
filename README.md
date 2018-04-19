e615a1e1788a685b276a50209bd4e81ac27406cd
Candy <you@example.com>
2018-04-19 09:28:38 +0800

    travis ci

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[![Build Status](https://www.travis-ci.org/zp1112/aegis-utils.svg?branch=master)](https://www.travis-ci.org/zp1112/aegis-utils)
+[![Coverage Status](https://coveralls.io/repos/github/zp1112/aegis-utils/badge.svg?branch=master)](https://coveralls.io/github/zp1112/aegis-utils?branch=master)
+
 ```shell
 npm i
 ```
--- index.js
+++ index.js
@@ -10,5 +10,19 @@ module.exports = {
     });
     a.map(row => row.value === Math.max(...a.map(row => row.value)) ? e.push(row.key) : null);
     return e;
-  },
+  }
 }
+// import countup from 'countup';
+//
+// const countNum = (str) => {
+//   const e = [];
+//   const a = [...new Set(str.replace(/\s+/g, '').split(''))].map((row) => {
+//     const reg = new RegExp(row, 'g');
+//     return {
+//       key: row,
+//       value: str.match(reg).length
+//     };
+//   });
+//   a.map(row => row.value === Math.max(...a.map(row => row.value)) ? e.push(row.key) : null);
+//   return e;
+// }
